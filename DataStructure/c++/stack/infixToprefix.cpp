98ffd3e0d8a49cfbbedf9890fdc5f7bb31bfafc0
Subham Sharma <83713146+subhamengine@users.noreply.github.com>
2021-09-13 19:25:10 +0530

    Create infixToprefix.cpp

---
+++ DataStructure/c++/stack/infixToprefix.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int prec(char c){
+  if(c=='^') return 3;
+  else if(c=='/' or c=='*') return 2;
+  else if(c=='+' or c=='-') return 1;
+  else return -1;
+}
+string rev(string s){
+  string res;
+  for(int i=s.length()-1 ;i>=0 ; i--){
+    res+=s[i];
+  }
+  return res;
+}
+
+string infixtoprefix(string s){
+  stack<char>st;
+  string res;
+  for(int i=0 ; i<s.length() ;i++){
+    if(s[i]=='(') s[i]=')';
+    else if(s[i]==')') s[i]='(';
+  }
+  for(int i=0 ; i < s.length() ; i++){
+    if(s[i] >='a' and s[i] <='z' || s[i] >= 'A' and s[i] <= 'Z'){
+      res+=s[i];
+    }
+    else if(s[i] == '('){
+      st.push(s[i]);
+    }
+    else if(s[i] == ')'){
+      while(!st.empty() and st.top() != '('){
+        res+=st.top();
+        st.pop();
+      }
+      if(!st.empty()) st.pop();
+    }
+    else{
+      while(!st.empty() and prec(st.top()) > prec(s[i])){
+        res+=st.top();
+        st.pop();
+      }
+      st.push(s[i]);
+    }
+  }
+  while(!st.empty()){
+    res+=st.top();
+    st.pop();
+  }
+  return rev(res);
+}
+int main() 
+{
+    cout<<infixtoprefix(rev("(a-b/c)*(a/k-l)"));
+    return 0;
+}
