4ce19fa8bef8509a734f6b0328647e1f3bd2cb14
Subham Sharma <83713146+subhamengine@users.noreply.github.com>
2021-09-13 19:53:54 +0530

    Create boundary-traversal.cpp

---
+++ DataStructure/c++/Binary-trees/boundary-traversal.cpp
@@ -0,0 +1,46 @@
+bool isleaf(Node* root){
+  if(!root->left and !root->right) return true;
+  else return false;
+}
+
+void addleftboundary(Node* root , vector<int>&res){
+  Node* cur = root->left;
+  while(cur){
+    if(!isleaf(cur)) res.push_back(cur->val);
+    if(cur->left) cur = cur->left;
+    else cur = cur->right;
+  }
+}
+
+void addrightboundary(Node* root , vector<int>&res){
+  Node* cur = root->right;
+  vector<int>temp;
+  while(cur){
+    if(!isleaf(cur)) temp.push_back(cur->val);
+    if(cur->right) cur = cur->right;
+    else cur = cur->left;
+  }
+  for(int i = temp.size()-1 ; i >= 0 ; i--){
+    res.push_back(temp[i]);
+  }
+}
+
+void addleaves(Node* root , vector<int>&res){
+  if(isleaf(root)){
+    res.push_back(root->val);
+    return;
+  } 
+  if(root->left) addleaves(root->left , res);
+  if(root->right) addleaves(root->right , res);
+  
+}
+
+vector<int>printboundary(Node* root){
+  vector<int>res;
+  if(!root) return res;
+  if(!isleaf(root)) res.push_back(root->val);
+  addleftboundary(root , res);
+  addleaves(root,res);
+  addrightboundary(root , res);
+  return res;
+}
