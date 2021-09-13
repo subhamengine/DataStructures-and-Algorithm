f95a4293fab44c5435aa46c511e75e472e4b3fd5
Subham Sharma <83713146+subhamengine@users.noreply.github.com>
2021-09-13 20:04:27 +0530

    Create Reverse a Linked List in groups of given size.cpp

---
+++ DataStructure/c++/Linked-list/Reverse a Linked List in groups of given size.cpp
@@ -0,0 +1,21 @@
+Reverse a Linked List in groups of given size. : https://practice.geeksforgeeks.org/problems/reverse-a-linked-list-in-groups-of-given-size/1
+solution:
+
+struct node *reverse (struct node *head, int k)
+    { 
+        Node* curr = head;
+        Node* nex;
+        Node* prev = NULL;
+        int c = 0;
+        while(curr!=NULL and c<k){
+            nex = curr->next;
+            curr->nex = prev;
+            prev = curr;
+            curr = nex;
+            c++;
+        }
+        if(nex!=NULL){
+            head->next = revers(next,k);
+        }
+        return prev;
+    }
