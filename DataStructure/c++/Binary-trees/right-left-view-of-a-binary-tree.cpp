26f660b0a8ed8eaef6ca3d89d158e0f51b72ceff
Subham Sharma <83713146+subhamengine@users.noreply.github.com>
2021-09-15 12:55:57 +0530

    Create right-left-view-of-a-binary-tree.cpp

---
+++ DataStructure/c++/Binary-trees/right-left-view-of-a-binary-tree.cpp
@@ -0,0 +1,20 @@
+//the last node of each level is the rightmost node
+//will be doing a reverse of preorfer traversal(root - right - left)
+
+void process(TreeNode* root , int level , vector<int>&res){
+  
+  if(root == NULL) return;
+  if(res.size() == level) res.push_back(root->val);
+  process(root->right , level+1 , res);
+  process(root->left , level+2 , res);
+  
+}
+
+vector<int>rightview(TreeNode* root){
+  
+  vector<int>res;
+  if(!root) return ans;
+  process(root,0,res);
+  return res;
+  
+}
