d3ba9c5efc0966e6e13d9f4206d6e295a9437471
Subham Sharma <83713146+subhamengine@users.noreply.github.com>
2021-09-16 20:42:08 +0530

    Create max-width-of-binarytree.cpp

---
+++ DataStructure/c++/Binary-trees/max-width-of-binarytree.cpp
@@ -0,0 +1,25 @@
+int widthofbinarytree(TreeNode* root){
+  
+  if(!root) return 0;
+  int ans = 0;
+  queue<pair<TreeNode* , int>>q;
+  q.push({root , 0});
+  while(!q.empty()){
+    int size = q.size();
+    int mini = q.front().second;
+    int first , last;
+    
+    for(int i = 0 ; i < size ; i++){
+      int cur_id = q.front().second - mini;
+      TreeNode* node = q.front().first;
+      q.pop();
+      if(i == 0) first = cur_id;
+      if(i == size-1) last = cur_id;
+      
+      if(node->left) q.push({node->left , cur_id*2+1});
+      if(node->right) q.push({node->right , cur_id*2+2});
+    }
+    ans = max(ans , last-first+1);
+  }
+  return ans;
+}
