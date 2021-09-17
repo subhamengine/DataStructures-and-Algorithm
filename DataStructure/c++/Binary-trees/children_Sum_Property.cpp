74654b2e82e0eb07bf0f5b5f6108eb044b3ea4cb
Subham Sharma <83713146+subhamengine@users.noreply.github.com>
2021-09-17 18:48:13 +0530

    Create children_Sum_Property.cpp

---
+++ DataStructure/c++/Binary-trees/children_Sum_Property.cpp
@@ -0,0 +1,20 @@
+void childrenSumProperty(TreeNode* root){
+  if(!root) return;
+  int child = 0;
+  if(root->left) child += root->left->data;
+  if(root->right) child += root->right->data;
+  
+  if(child >= root->data) root->data = child;
+  else{
+    if(root->left) root->left->data = root->data;
+    else if(root->right) root->right->data = root->data;
+  }
+  
+  childrenSumProperty(root->left);
+  childrenSumProperty(root->right);
+  
+  int tot = 0;
+  if(root->left) tot += root->left->data;
+  if(root->right) tot += root->right->data;
+  if(root->left or root->right) root->data = tot;
+}
