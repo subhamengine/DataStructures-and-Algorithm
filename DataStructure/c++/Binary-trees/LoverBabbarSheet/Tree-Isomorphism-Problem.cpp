9134f4a5011c9274c8b06c11bb46afc9802ed00d
Subham Sharma <83713146+subhamengine@users.noreply.github.com>
2021-10-20 19:50:32 +0530

    Create Tree-Isomorphism-Problem.cpp

---
+++ DataStructure/c++/Binary-trees/LoverBabbarSheet/Tree-Isomorphism-Problem.cpp
@@ -0,0 +1,12 @@
+class Solution{
+  public:
+    bool isIsomorphic(Node *root1,Node *root2)
+   {
+       if(!root1 && !root2) return true;
+       if(!root1 || !root2) return false;
+       if(root1->data!=root2->data) return false;
+       bool a=isIsomorphic(root1->left,root2->left)&&isIsomorphic(root1->right,root2->right);
+       bool b=isIsomorphic(root1->left,root2->right)&&isIsomorphic(root1->right,root2->left);
+       return a or b;
+   }
+};
