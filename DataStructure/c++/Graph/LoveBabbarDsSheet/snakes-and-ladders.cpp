9ae0043965ca2c8503706114486d316b2c35531b
Subham Sharma <83713146+subhamengine@users.noreply.github.com>
2021-11-22 00:44:04 +0530

    Update snakes-and-ladders.cpp

--- DataStructure/c++/Graph/LoveBabbarDsSheet/snakes-and-ladders.cpp
+++ DataStructure/c++/Graph/LoveBabbarDsSheet/snakes-and-ladders.cpp
@@ -1,3 +1,4 @@
+//leetcode
 class Solution {
 public:
     void findcordinatesR(int curr , int n , int &ro , int &co){
@@ -47,4 +48,43 @@ public:
         return -1;
     }
     
+};
+//geeksforgeeks
+class Solution{
+public:
+    int minThrow(int N, int arr[]){
+        vector<int>mainarr(31,-1);
+        int steps = 0;
+        int low,high;
+        low = 0,high = 1;
+        for(int i = 0 ; i < N ; i++){
+            mainarr[arr[low]] = arr[high];
+            low = low + 2;
+            high = high + 2;
+        }
+        bool vis[31] = {false};
+        queue<int>q;
+        q.push(1);
+        vis[1] = true;
+        while(!q.empty()){
+            int size = q.size();
+            for(int i = 0 ; i < size ; i++){
+                int x = q.front();
+                q.pop();
+                if(x == 30) return steps;
+                for(int k = 1 ; k <= 6 ; k++){
+                    if(k + x > 30) break;
+                    if(vis[k+x] == true) continue;
+                    vis[k+x] = true;
+                    if(mainarr[k+x] == -1){
+                        q.push(k+x);
+                    }else{
+                        q.push(mainarr[k+x]);
+                    }
+                }
+            }
+            steps++;
+        }
+        return steps;
+    }
 };
