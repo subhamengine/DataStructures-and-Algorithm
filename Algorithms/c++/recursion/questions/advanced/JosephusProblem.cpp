a56858c2a0a4598a53d737ab94e88a5b1cccb102
Subham Sharma <83713146+subhamengine@users.noreply.github.com>
2021-12-27 02:05:00 +0530

    Update JosephusProblem.cpp

--- Algorithms/c++/recursion/questions/advanced/JosephusProblem.cpp
+++ Algorithms/c++/recursion/questions/advanced/JosephusProblem.cpp
@@ -11,4 +11,21 @@ int main()
   cin>>n>>k;
     cout << jos(n,k);
     return 0;
-}                                                                                                                                                                                                                                                   
+}   
+//leetcode
+class Solution {
+public:
+    int help(int n , int k){
+        if(n == 1) return 0;
+        return(help(n-1,k)+k)%n;
+    }
+    int findTheWinner(int n, int k) {
+        int arr[n];
+        for(int i = 0 ; i < n ; i++){
+            arr[i] = i+1;
+        }
+        int ans = help(n,k);
+        return arr[ans];
+    }
+    
+};
